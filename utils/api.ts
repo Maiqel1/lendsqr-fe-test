fba4027e49a628048e5423b7db07ea6d73490e91
maiqel <michaelolowe321@gmail.com>
2024-11-01 17:25:24 +0100

    feat: modified api call to fetch from db instead of json server locally

--- utils/api.ts
+++ utils/api.ts
@@ -1,10 +1,14 @@
-export const API_URL = "http://localhost:3001";
+export const API_URL =
+  "https://raw.githubusercontent.com/Maiqel1/mockdb/main/db.json";
 
 export const fetchUsers = async () => {
   try {
-    const response = await fetch(`${API_URL}/users`);
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
-    return data;
+    return data.users;
   } catch (error) {
     console.error("Error fetching users:", error);
     throw error;
